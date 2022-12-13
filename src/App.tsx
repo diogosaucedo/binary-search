e0b9d70ce0619cb9f90837b9daad6a4881f320dd
Diogo Ferreira Saucedo <Diogoferreiradfs@hotmail.com>
2022-12-13 11:48:29 -0400

    feat(app): add binary search
    App.tsx contains the rules for the binary search and with them assembles the components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,73 @@
 import React from 'react';
+import Button from './components/Form/Button';
+import Center from './components/Layout/Center';
+import Stack from './components/Layout/Stack';
+import Text from './components/Typography/Text';
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
+  // START, RUNNING, END
+  const [state, setState] = React.useState('START');
+
+  // guess
+  const [guess, setGuess] = React.useState(150);
+  const [guessCounter, setGuessCounter] = React.useState(0);
+
+  // range
+  const [min, setMin] = React.useState(0);
+  const [max, setMax] = React.useState(300);
+
+  const startGame = () => {
+    setState('RUNNING');
+    setMin(0);
+    setMax(300);
+    setGuessCounter(1);
+    setGuess(150);
+  };
+
+  if (state === 'START') {
+    return <Button onClick={startGame}>Start Game</Button>;
+  }
+
+  const less = () => {
+    setGuessCounter(guessCounter + 1);
+    setMax(guess);
+    const nextGuess = Math.floor((guess - min) / 2) + min;
+    setGuess(nextGuess);
+  };
+
+  const greater = () => {
+    setGuessCounter(guessCounter + 1);
+    setMin(guess);
+    const nextGuess = Math.floor((max - guess) / 2) + guess;
+    setGuess(nextGuess);
+  };
+
+  const win = () => {
+    setState('END');
+  };
+
+  if (state === 'END') {
+    return (
+      <Center>
+        <Text>
+          I hit number {guess} with {guessCounter} guesses!
+        </Text>
+        <Button onClick={startGame}>Start Game</Button>
+      </Center>
+    );
+  }
   return (
     <div className="App">
       <GlobalStyles />
-      <header>
-        <h1>Hello World!</h1>
-      </header>
+      <Center>
+        <Text>Is your number {guess}?</Text>
+        <Stack gap="8px">
+          <Button onClick={less}>Less</Button>
+          <Button onClick={win}>Win</Button>
+          <Button onClick={greater}>Greater</Button>
+        </Stack>
+      </Center>
     </div>
   );
 }
